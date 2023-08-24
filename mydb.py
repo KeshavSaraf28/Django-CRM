725e108be11e9d552d8d0fc17341fe9d8ad2405a
Keshav Saraf <keshavsaraf27@gmail.com>
2023-08-24 13:54:22 +0530

    initial commit

---
+++ mydb.py
@@ -0,0 +1,9 @@
+import mysql.connector
+dataBase = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="Ke@280902")
+
+cursorObject=dataBase.cursor()
+cursorObject.execute("CREATE DATABASE mydatabase")  
+print("All Done!")
