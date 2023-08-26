75761eea31ecd35561c2e9217eb42a04b62992c4
Keshav Saraf <keshavsaraf27@gmail.com>
2023-08-26 16:40:13 +0530

    half project

--- website/views.py
+++ website/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from .forms import SignUpForm
+from .models import Record
+ 
+def home(request):
+    records=Record.objects.all()
 
-# Create your views here.
+    if request.method=="POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,"You have been Logged In!")
+            return redirect('home')
+        else:
+            messages.success(request,"Error Logging In!")
+            return redirect('home')
+    else:
+        return render(request,"home.html",{'records':records})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out...")
+    return redirect('home')
+
+def customer_record(request,pk):
+    if request.user.is_authenticated:
+        customer_record=Record.objects.get(id=pk)
+        return render(request,'record.html',{'customer_record':customer_record})
+    else:
+        messages.success(request,"You must be logged in to view that page!")
+        return redirect('home')
+    
+
+def register_user(request):
+    if request.method == 'POST':
+        form=SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data["username"]
+            password=form.cleaned_data["password1"]
+            user=authenticate(request,username=username,password=password)
+            login(request,user)
+            messages.success(request,"You have successfully Registered!")
+            return redirect('home')
+    else:
+        form=SignUpForm()
+        return render(request, 'register.html',{'form':form})
+    return render(request, 'register.html',{'form':form})
